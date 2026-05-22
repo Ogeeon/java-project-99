72d4f5b782fdb88ce9b131e37da56b8b30ffa759
Alexey Bogatyrev <avbogatyrev@gmail.com>
2026-05-22 16:53:03 +1000

    Fix as per SonarQube's recommendation

--- src/main/java/hexlet/code/app/model/TaskStatus.java
+++ src/main/java/hexlet/code/app/model/TaskStatus.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Setter
 @Getter
 @Entity
-@ToString(includeFieldNames = true, onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
 @EntityListeners(AuditingEntityListener.class)
 @Table(name = "taskstatuses")
 public class TaskStatus implements BaseEntity {
