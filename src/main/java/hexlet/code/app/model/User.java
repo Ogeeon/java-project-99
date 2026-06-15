32feb32ea4d4c990ba451b6190c03655ffc74879
Alexey Bogatyrev <avbogatyrev@gmail.com>
2026-06-15 15:28:57 +1000

    Remove unnecessary overrides

--- src/main/java/hexlet/code/app/model/User.java
+++ src/main/java/hexlet/code/app/model/User.java
@@ -57,24 +57,4 @@ public class User implements UserDetails, BaseEntity {
     public String getUsername() {
         return email;
     }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
