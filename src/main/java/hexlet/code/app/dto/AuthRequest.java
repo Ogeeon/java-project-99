e02196d446ae0f4b050b21d39c44c2cda290440b
Alexey Bogatyrev <avbogatyrev@gmail.com>
2026-06-15 14:10:31 +1000

    Add validation of AuthRequest

--- src/main/java/hexlet/code/app/dto/AuthRequest.java
+++ src/main/java/hexlet/code/app/dto/AuthRequest.java
@@ -1,11 +1,15 @@
 package hexlet.code.app.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class AuthRequest {
+    @NotBlank
     private String username;
+
+    @NotBlank
     private String password;
 }
